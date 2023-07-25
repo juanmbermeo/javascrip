9e897dbcddfec2be8444984bf83a9588d2337394
juanmbermeo <bermeojuanmanuel1@gmail.com>
2023-07-25 15:05:42 -0500

    se creo index y js

---
+++ js/script.js
@@ -0,0 +1,57 @@
+//console.log("Adso");
+//alert("Adso");
+//document.write("Adso");
+//var edad = 33;
+//const edad2=22;
+//let edad3=33;
+
+//var edad = 33; 
+//alert("mi edad es: " +edad);
+//const nom="jJesus";
+//let num=2.5;
+//console.log(typeof num);
+//bolean='ADSO';
+//console.log(typeof bolean);
+
+/*const nom="Daniel"
+const ape='Fernandez'
+alert("Mi nombre es "+nom+" y mi apellido es "+ape)*/
+
+//Operadores Aritmeticos
+/*const num1=2
+const num2=11
+alert("La suma es: "+parseInt(num1) +parseInt(num2))*/
+
+/*const num1=5
+const num2=2.5
+const resta= num1-num2
+parseInt (resta)
+alert("La resta es " +resta)*/
+
+/*const num1=5
+const num2=2.5
+const multi= num1-num2
+alert("La division es " +multi)*/
+
+/*const num1=5
+const num2=2.5
+const divi= num1/num2
+alert("La Division es " +divi)*/
+
+/*let nom=prompt("Escribe el nombre")
+alert("Mi nombre es: "+nom)*/
+
+/*let nom=prompt("Escribe el nombre")
+alert("Mi nombre es: "+nom)*/
+
+
+//mostrar en una alerta con los nombres y apellidos completos, edad, peso y dirrecccion.
+
+
+let nom= prompt("Ingresa tu nombre:")
+let apell= prompt("Ingresa tu apellido:")
+let edad= prompt("Ingresa tu edad:")
+let peso= prompt("Ingresa tu peso en kg:")
+let direc= prompt("Ingresa tu dirección:")
+
+alert("Soy "+nom+" "+apell+ " tengo" +edad+ " años, mi peso es de "+peso+"kg y vivo en "+direc)
